0bf657cbd03d167173c9afd97ac2d95480c89419
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:26 -0500

    xml commenting

--- src/Soenneker.Netlify.OpenApiClientUtil/Abstract/INetlifyOpenApiClientUtil.cs
+++ src/Soenneker.Netlify.OpenApiClientUtil/Abstract/INetlifyOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Netlify.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface INetlifyOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<NetlifyOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
